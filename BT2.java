187248ba277b38008a87edc71992d8865bf763b9
Nguyễn Quang Tâm <151425689+TamNguyen100805@users.noreply.github.com>
2024-08-22 23:33:20 +0700

    Add files via upload

---
+++ BT2.java
@@ -0,0 +1,11 @@
+package pheptinh;
+import java.util.Scanner;
+public class BT2 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        //nhap
+        System.out.println("Nhap: ");
+        boolean a = kb.hasNext();
+        System.out.println("Co phai chuoi khong: " + a);
+    }
+}
---
+++ BT3.java
@@ -0,0 +1,13 @@
+package pheptinh;
+import java.util.Scanner;
+public class BT3 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        //nhap
+        System.out.println("Nhap: ");
+        boolean a = kb.hasNextInt();
+        System.out.println("Co phai so nguyen khong: " + a);
+    }
+}
+
+}
